5d290f184f22cd25d5b299829bc4d1d1e4cfd6c5
jasperredis <158768468+Jasperredis@users.noreply.github.com>
2024-12-15 16:33:34 -0600

    Create rpm.sh

---
+++ linux-installs/rpm.sh
@@ -0,0 +1,44 @@
+# NOTE!
+# In the packaged release, this would be in the main project directory under the name "install.sh".
+
+echo "Checking if dependencies are installed..."
+check_dependency() {
+    if rpm -q $PACKAGE_NAME &>/dev/null; then
+        echo "$PACKAGE_NAME is installed!"
+    else
+        echo "$PACKAGE_NAME is not installed! It is required for Lifeline.py. Install? (y/N)"
+        read -p "> " answer
+        answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+        if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+        echo "Installing $PACKAGE_NAME..."
+        sudo dnf install -y "$PACKAGE_NAME"
+        elif [[ "$answer" == "n" || "$answer" == "no" ]]; then
+            echo "Installation of $PACKAGE_NAME cancelled."
+        else
+            echo "Invalid input."
+            exit
+        fi
+    fi
+}
+
+PACKAGE_NAME="python3"
+check_dependency()
+PACKAGE_NAME="python-requests"
+check_dependency()
+PACKAGE_NAME="python-pygame"
+check_dependency()
+PACKAGE_NAME="python-colorama"
+check_dependency()
+echo "While it is not required, it is recommended to ensure that the Python library, plyer, is installed on your system. Proceed? (y/N)"
+read -p "> " answer
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+    echo "Okay!"
+elif [[ "$answer" == "n" || "$answer" == "no" ]]; then
+    echo "Cancelled."
+else
+    echo "Invalid input."
+    exit
+fi
+
+echo "Great! Now run the command in 'run.sh' (or run the file directly) to play Lifeline.py!"
